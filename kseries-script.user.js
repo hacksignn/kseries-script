b321bf1b83d4c0cfd68f3a91621474266df153af
nithinan woranitikul <nithinanw@wisesoft.co.th>
2018-07-02 11:35:03 +0700

    fix indent

--- kseries-script.user.js
+++ kseries-script.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name          	kseries-script
+// @name            kseries-script
 // @description     user script for use with kseries.co
-// @author			Houndy Dreamer
+// @author          Houndy Dreamer
 // @namespace       https://github.com/hacksignn
 // @include         http://www.kseries.co/clip/*
-// @match			http://*.example.com/clip/*
+// @match           http://*.example.com/clip/*
 // @require         https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @version         0.1
 // ==/UserScript==
